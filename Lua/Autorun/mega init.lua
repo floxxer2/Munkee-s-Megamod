9c2130325bd224668fd9a7ce05d0664da57219b2
munkee <118648083+floxxer2@users.noreply.github.com>
2025-12-07 22:31:49 -0500

    syntactic sugar

--- Lua/Autorun/mega init.lua
+++ Lua/Autorun/mega init.lua
@@ -23,7 +23,7 @@ Megamod.Admins = {
 Megamod.Omni = {}
 
 Megamod.BlacklistedPlayerMonsters = {
-    "Truebeast"
+    Truebeast = true
 }
 
 -- Static/Dynamic/Kinematic
@@ -61,7 +61,7 @@ if CLIENT then
         dofile(Megamod_Client.Path .. "/Lua/client/hunt.lua")
         dofile(Megamod_Client.Path .. "/Lua/client/controlpanel.lua") -- This has the side-effect of making the "control panel" option the last in the list
         dofile(Megamod_Client.Path .. "/Lua/client/dimelocator.lua")
-        --dofile(Megamod_Client.Path .. "/Lua/client/monster.lua")
+        dofile(Megamod_Client.Path .. "/Lua/client/monster.lua")
     end, 1)
 end
 
