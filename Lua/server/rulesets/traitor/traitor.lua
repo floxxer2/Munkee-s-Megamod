9849b03c429dbaac28d2dd0f799a45606d2158ff
munkee <118648083+floxxer2@users.noreply.github.com>
2025-12-25 15:49:40 -0500

    remove debug thing

--- Lua/server/rulesets/traitor/traitor.lua
+++ Lua/server/rulesets/traitor/traitor.lua
@@ -1628,9 +1628,9 @@ function rs.SetTraitor(client, brainwashed)
             [4] = 0, -- Can be negative; timer (counting up) representing the Lender's dwindling patience, if too high, this traitor may be the target of another traitor
             [5] = false, -- If the traitor is in a "Meeting" objective
             [6] = 0, -- The traitor's accumulated "Credit," used for bragging rights in the end-of-round summary
-            [7] = 1, -- Cooldown for objectives, [4] doesn't go up while this is >0 and you can't get another objective either; starts at 2.5 minutes
+            [7] = 150, -- Cooldown for objectives, [4] doesn't go up while this is >0 and you can't get another objective either; starts at 2.5 minutes
             [8] = false, -- If the traitor has received the "you can get another objective" message
-        } -- #DEBUG#
+        }
     }
     for admin in Client.ClientList do
         if Megamod.Admins[admin.SteamID] then
